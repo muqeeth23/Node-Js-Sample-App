5d8f9f2972f49b672cb7707eade73eda4c421a7c
muqeeth23 <46218921+muqeeth23@users.noreply.github.com>
2022-05-26 12:18:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:blue;">Happy New Year - Rakesh- 2022 </h1> \n');
+  res.send('<h1 style="color:blue;">Node JS sample app </h1> \n');
 });
 
 app.listen(PORT, HOST);
